ef16c34589d89504c3831372778ec8c4b8e88ab0
MordeMain <cmgiraldo98@gmail.com>
2026-09-26 15:28:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aetheris-v15';
+const CACHE = 'aetheris-v16';
 const FILES = ['./', './index.html', './pc.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n))))); self.clients.claim(); });
